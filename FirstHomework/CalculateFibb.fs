f1ae7db0a4c6046c8ef622757abda6e98ed68b69
Alexander Magikov <alexmagikov228@gmail.com>
2026-04-23 22:58:43 +0300

    fix

--- FirstHomework/CalculateFibb.fs
+++ FirstHomework/CalculateFibb.fs
@@ -1,13 +1,16 @@
 module CalculateFibb
 
 let calculateFibb num =
-    if num < 0 then
-        Error "Num must be positive"
-    else
-        let rec fibb current previous stepsLeft =
-            if stepsLeft = 0 then
-                current
-            else
-                fibb (current + previous) current (stepsLeft - 1)
-        Ok (fibb 0 1 num)
+    match num with
+    | n when n < 0 -> Error "Num must be positive"
+    | _ -> 
+        let rec fibb = function
+            | 0 -> Ok 0
+            | 1 -> Ok 1
+            | n ->
+                match fibb (n - 1), fibb ( n - 2) with
+                | Ok a, Ok b -> Ok (a + b)
+                | Error e, _ -> Error e
+                | _, Error e -> Error e
+        fibb num
        

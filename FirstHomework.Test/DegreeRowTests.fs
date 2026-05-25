0846f4af8d73bd1698d57b87950e969e658b96a6
Alexander Magikov <alexmagikov228@gmail.com>
2026-05-25 18:52:45 +0300

    fix: fix remarks

--- FirstHomework.Test/DegreeRowTests.fs
+++ FirstHomework.Test/DegreeRowTests.fs
@@ -6,16 +6,25 @@ open NUnit.Framework
 
 [<Test>]
 let ``DegreeRow for normal value should return normal value`` () =
-    degreeRow 2 3 |> should equal [4; 8; 16; 32]
+    degreeRow 2 3 |> should equal (Ok [4.0; 8.0; 16.0; 32.0] : Result<float list, string>)
     
 [<Test>]
 let ``DegreeRow for n=0 and m = 1 should return [1; 2]`` () =
-    degreeRow 0 1 |> should equal [1; 2]
+    degreeRow 0 1 |> should equal (Ok [1.0; 2.0] : Result<float list, string>)
     
 [<Test>]
 let ``DegreeRow for n=1 and m = 0 should return [2]`` () =
-    degreeRow 1 0 |> should equal [2]
+    degreeRow 1 0 |> should equal (Ok [2.0] : Result<float list, string>)
     
 [<Test>]
 let ``DegreeRow for n=0 and m = 0 should return [1]`` () =
-    degreeRow 0 0 |> should equal [1] 
+    degreeRow 0 0 |> should equal (Ok [1.0] : Result<float list, string>)
+    
+[<Test>]
+let ``DegreeRow for negative n should return fractional values`` () =
+    degreeRow -2 3 |> should equal (Ok [0.25; 0.5; 1.0; 2.0] : Result<float list, string>)
+    
+[<Test>]
+let ``DegreeRow for negative m should return Error`` () =
+    degreeRow 2 -1
+    |> should equal (Error "m must be non-negative" : Result<float list, string>)
--- FirstHomework/DegreeRow.fs
+++ FirstHomework/DegreeRow.fs
@@ -2,11 +2,11 @@ module DegreeRow
 
 // Calculate the list with format: [2^n; 2^(n+1); ...; 2^(n+m)].
 let degreeRow (n : int) (m : int)=
-    let rec loop current_list current_element index =
-        match index with
-        | i when i > m -> current_list
-        | _ -> loop (current_element::current_list) (current_element >>> 1) (index + 1)
-    
     match m with
-    | num when num < 0 -> []
-    | _ -> loop [] (1 <<< (n + m)) 0
+    | num when num < 0 -> Error "m must be non-negative"
+    | _ ->
+        let rec loop rem currentElement currentList =
+            match rem with
+            | 0 -> Ok (List.rev currentList)
+            | _ -> loop (rem - 1) (currentElement * 2.0) (currentElement :: currentList)    
+        loop (m + 1) (2.0 ** float n) []
